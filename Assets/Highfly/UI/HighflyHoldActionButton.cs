f9a65faae6774600d02ff9c217360296235567c5
drakentzy07 <lucaascabral07@gmail.com>
2026-08-25 19:25:59 -0300

    Add hold/release input component for block and charge actions

---
+++ Assets/Highfly/UI/HighflyHoldActionButton.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Highfly.UI
+{
+    public sealed class HighflyHoldActionButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+        [SerializeField] private UnityEvent onPressed;
+        [SerializeField] private UnityEvent onReleased;
+
+        private bool _held;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_held)
+                return;
+            _held = true;
+            onPressed?.Invoke();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Release();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_held)
+                Release();
+        }
+
+        private void OnDisable()
+        {
+            if (_held)
+                Release();
+        }
+
+        private void Release()
+        {
+            if (!_held)
+                return;
+            _held = false;
+            onReleased?.Invoke();
+        }
+    }
+}
